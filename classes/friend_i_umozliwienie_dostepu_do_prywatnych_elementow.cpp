ac5cac4d50e2f1a87d1cfc55cbbde3d6726e3e00
rebixpl <33410494+rebixpl@users.noreply.github.com>
2019-12-04 15:49:21 +0100

    Update and rename friend_i_umozliwienie_zewnetrznej_FUNKCJI_dostep_do_prywatnych_elementow.cpp to friend_i_umozliwienie_dostepu_do_prywatnych_elementow.cpp

--- classes/friend_i_umozliwienie_dostepu_do_prywatnych_elementow.cpp
+++ classes/friend_i_umozliwienie_dostepu_do_prywatnych_elementow.cpp
@@ -8,16 +8,26 @@ class Human
 private:
     string Name;
     int Age;
-    
-    //Deklaracja przyjaciela informuje kompilator, że funkcja DisplayAge ma specjalne
-    //uprawnienia dostępu do prywatnych elementów składowych
+
+    //Deklaracja przyjaciela informuje kompilator, że funkcja DisplayAge oraz klasa Utility
+    //ma specjalne uprawnienia dostępu do prywatnych elementów składowych
     friend void DisplayAge(const Human& inputClass);
+    friend class Utility;
 
 public:
     Human(string inputName, int inputAge)
     {
         this->Name = inputName;
         this->Age = inputAge;
+    }
+};
+
+class Utility
+{
+public:
+    static void DisplayAge(const Human& inputClass)
+    {
+        cout << inputClass.Age << "\n";
     }
 };
 
@@ -29,9 +39,12 @@ void DisplayAge(const Human& inputClass)
 int main()
 {
     Human FirstMan("Adam", 21);
-    
+
     cout << "Uzyskanie dostepu do prywatnego elementu skladowego Age przy uzyciu slowa kluczowego friend: ";
     DisplayAge(FirstMan);
+
+    cout << "\n" << "Uzyskanie dostepu do prywatnego elementu skladowego Age przy uzyciu friend class: ";
+    Utility::DisplayAge(FirstMan);
 
     return 0;
 }
