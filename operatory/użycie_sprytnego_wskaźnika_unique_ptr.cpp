2fefa91810ba4f221ff79280164ff09f1f816e2d
rebixpl <33410494+rebixpl@users.noreply.github.com>
2019-12-16 21:56:13 +0100

    Create użycie_sprytnego_wskaźnika_unique_ptr.cpp

---
+++ operatory/użycie_sprytnego_wskaźnika_unique_ptr.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory> // To polecenie jest konieczne w celu użycia std::unique_ptr
+#include <string>
+
+using namespace std;
+
+class Date
+{
+private:
+    int Day;
+    int Month;
+    int Year;
+
+    string DateInString;
+
+public:
+    Date(int inputDay, int inputMonth, int inputYear)
+        :Day(inputDay), Month(inputMonth), Year(inputYear) {};
+
+    void DisplayDate()
+    {
+        cout << Day << " / " << Month << " / " << Year << "\n";
+    }
+};
+
+int main()
+{
+    unique_ptr<int> pDynamicAllocInt(new int);
+    *pDynamicAllocInt = 42;
+
+    //użycie sprytnego wskaźnika typu, takiego jak int*
+    cout << "Liczba calkowita to: " << *pDynamicAllocInt << "\n";
+
+    unique_ptr<Date> pHoliday(new Date(25,12,2019));
+    cout << "Nowy egzemplarz daty zawiera: ";
+
+    // Użycie pHoliday w taki sposób, jakby to był Date*.
+    pHoliday->DisplayDate();
+
+    // Podczas używania unique_ptr poniższe polecenia są zbędne:
+    // delete pDynamicAllocInteger;
+    // delete pHoliday;
+
+    return 0;
+}
