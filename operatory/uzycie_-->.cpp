017fce2c7a9c2358371e522f665030c6d8205655
rebixpl <33410494+rebixpl@users.noreply.github.com>
2020-04-01 11:26:50 +0200

    Create uzycie_-->.cpp

---
+++ operatory/uzycie_-->.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int startValue = 1000, endValue = 10;
+
+int main(){
+    int iNumer = startValue+1;
+    while(iNumer-->endValue){
+        std::cout << iNumer << std::endl;
+    } 
+}
